da7ccf51831593be423ba2ccc6fc65a48d6070f2
Lucas Katayama <lucaskatayama@gmail.com>
2021-08-24 10:37:21 -0300

    fix: fmt

--- v3/status/status.go
+++ v3/status/status.go
@@ -1,10 +1,12 @@
 package status
 
+// Status represents a status
 type Status struct {
 	Code int    `json:"code"`
 	Msg  string `json:"msg"`
 }
 
+// New creates a status
 func New(status int, msg string) Status {
 	return Status{
 		Code: status,
